be23321cbeafdc00d2b2e97895ae9137b1fcef7d
Jordan <jordan@jordanlapp.com>
2014-07-29 14:42:17 -0500

    Moved swagger generation to the routes.php file

--- src/Jlapp/Swaggervel/routes.php
+++ src/Jlapp/Swaggervel/routes.php
@@ -16,5 +16,27 @@ Route::get(Config::get('swaggervel::app.doc-dir').'/{page?}', function($page='in
 });
 
 Route::get('api-docs', function() {
+    if (Config::get('app.debug')) {
+        $appdir = base_path()."/".Config::get('swaggervel::app.app-dir');
+        $docdir = base_path()."/".Config::get('swaggervel::app.doc-dir');
+
+        if (!\File::exists($docdir)) {
+            \File::makeDirectory($docdir);
+        }
+
+        if (is_writable($docdir)) {
+            $basepath = "";
+            $defaultBasePath = Config::get('swaggervel::app.default-base-path');
+            if ((isset($defaultBasePath)) && ($defaultBasePath !== '')) {
+                $basepath .= ' --default-base-path "'.$defaultBasePath.'"';
+            }
+            $result = shell_exec("php ".base_path()."/vendor/zircote/swagger-php/swagger.phar ".$appdir." -o ".$docdir.$basepath);
+
+            //display all swagger-php error messages so that it doesn't fail silently
+            if ((strpos($result, "[INFO]") != FALSE) || (strpos($result, "[WARN]") != FALSE)) {
+                throw new \Exception($result);
+            }
+        }
+    }
     return View::make('swaggervel::index');
 });
