e904e2d9ff76dfe6f804eb9af94a9394b3641389
Uroš Simović <uros313@gmail.com>
2016-01-07 09:35:00 +0100

    api-docs route added to config

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -10,25 +10,25 @@ header("Access-Control-Allow-Headers: X-Requested-With");
     <head>
         <meta charset="UTF-8">
         <title>Swagger UI</title>
-        <link rel="icon" type="image/png" href="vendor/swaggervel/images/favicon-32x32.png" sizes="32x32" />
-        <link rel="icon" type="image/png" href="vendor/swaggervel/images/favicon-16x16.png" sizes="16x16" />
-        <link href='vendor/swaggervel/css/typography.css' media='screen' rel='stylesheet' type='text/css'/>
-        <link href='vendor/swaggervel/css/reset.css' media='screen' rel='stylesheet' type='text/css'/>
-        <link href='vendor/swaggervel/css/screen.css' media='screen' rel='stylesheet' type='text/css'/>
-        <link href='vendor/swaggervel/css/reset.css' media='print' rel='stylesheet' type='text/css'/>
-        <link href='vendor/swaggervel/css/print.css' media='print' rel='stylesheet' type='text/css'/>
-        <script src='vendor/swaggervel/lib/jquery-1.8.0.min.js' type='text/javascript'></script>
-        <script src='vendor/swaggervel/lib/jquery.slideto.min.js' type='text/javascript'></script>
-        <script src='vendor/swaggervel/lib/jquery.wiggle.min.js' type='text/javascript'></script>
-        <script src='vendor/swaggervel/lib/jquery.ba-bbq.min.js' type='text/javascript'></script>
-        <script src='vendor/swaggervel/lib/handlebars-2.0.0.js' type='text/javascript'></script>
-        <script src='vendor/swaggervel/lib/underscore-min.js' type='text/javascript'></script>
-        <script src='vendor/swaggervel/lib/backbone-min.js' type='text/javascript'></script>
-        <script src='vendor/swaggervel/swagger-ui.js' type='text/javascript'></script>
-        <script src='vendor/swaggervel/lib/highlight.7.3.pack.js' type='text/javascript'></script>
-        <script src='vendor/swaggervel/lib/marked.js' type='text/javascript'></script>
+        <link rel="icon" type="image/png" href="/vendor/swaggervel/images/favicon-32x32.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="/vendor/swaggervel/images/favicon-16x16.png" sizes="16x16" />
+        <link href='/vendor/swaggervel/css/typography.css' media='screen' rel='stylesheet' type='text/css'/>
+        <link href='/vendor/swaggervel/css/reset.css' media='screen' rel='stylesheet' type='text/css'/>
+        <link href='/vendor/swaggervel/css/screen.css' media='screen' rel='stylesheet' type='text/css'/>
+        <link href='/vendor/swaggervel/css/reset.css' media='print' rel='stylesheet' type='text/css'/>
+        <link href='/vendor/swaggervel/css/print.css' media='print' rel='stylesheet' type='text/css'/>
+        <script src='/vendor/swaggervel/lib/jquery-1.8.0.min.js' type='text/javascript'></script>
+        <script src='/vendor/swaggervel/lib/jquery.slideto.min.js' type='text/javascript'></script>
+        <script src='/vendor/swaggervel/lib/jquery.wiggle.min.js' type='text/javascript'></script>
+        <script src='/vendor/swaggervel/lib/jquery.ba-bbq.min.js' type='text/javascript'></script>
+        <script src='/vendor/swaggervel/lib/handlebars-2.0.0.js' type='text/javascript'></script>
+        <script src='/vendor/swaggervel/lib/underscore-min.js' type='text/javascript'></script>
+        <script src='/vendor/swaggervel/lib/backbone-min.js' type='text/javascript'></script>
+        <script src='/vendor/swaggervel/swagger-ui.js' type='text/javascript'></script>
+        <script src='/vendor/swaggervel/lib/highlight.7.3.pack.js' type='text/javascript'></script>
+        <script src='/vendor/swaggervel/lib/marked.js' type='text/javascript'></script>
 
-        <script src='vendor/swaggervel/lib/swagger-oauth.js' type='text/javascript'></script>
+        <script src='/vendor/swaggervel/lib/swagger-oauth.js' type='text/javascript'></script>
 
         <!-- Some basic translations -->
         <!-- <script src='lang/translator.js' type='text/javascript'></script> -->
